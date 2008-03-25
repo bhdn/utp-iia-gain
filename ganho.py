99ba49e9b80b4300a9f190f9f8571d6d073e326d
Bogdano Arendartchuk <debogdano@gmail.com>
2008-03-25 08:19:47 -0300

    Added basic code to interact with the user

--- ganho.py
+++ ganho.py
@@ -8,10 +8,38 @@ import sys
 import os
 import csv
 
+def choose_ref_attr(cols):
+    while True:
+        print "Escolha o atributo de referencia:"
+        for i, col in enumerate(cols):
+            print "%d - %s" % (i, col)
+        default = len(cols) # the last columns is the default
+        rawchoice = raw_input("Qual destes? [%d]" % default)
+        if rawchoice:
+            try:
+                choice = int(rawchoice)
+            except ValueError:
+                print "Escolha invalida!"
+                continue
+        else:
+            choice = default
+        break
+    return choice - 1
+
+def output_attrs(cols):
+    pass
+
+def sort_by_gain(cols, entries, ref):
+    pass
+
 def work(args):
     for path in args:
+        print "Arquivo %s" % path
         reader = csv.reader(open(path))
-        columns = reader.next()
+        cols = reader.next()
+        choice = choose_ref_attr(cols)
+        sortedcols = sort_by_gain(cols, reader, choice)
+        output_attrs(sortedcols)
 
 if __name__ == "__main__":
-    work(sys.argv)
+    work(sys.argv[1:])
