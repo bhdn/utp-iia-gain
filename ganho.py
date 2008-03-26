40ad59a3a5bbb11d4c6bb5e86d4581698a403622
Bogdano Arendartchuk <bogdano@mandriva.com.br>
2008-03-26 15:46:53 -0300

    The first (crappy) working version of the algorithm!
    Based on the examples from:
    http://www2.dbd.puc-rio.br/pergamum/tesesabertas/0210488_04_cap_03.pdf
    
    The problem in this implementation is that it has a horrible representation
    of the data in memory. It uses hashes to count the frequency of a class on
    a class of the reference attribute. Python sucks!

--- ganho.py
+++ ganho.py
@@ -30,38 +30,69 @@ def choose_ref_attr(cols):
 def output_attrs(cols):
     print "Atributos ordenados por entropia:"
     for entropy, attr in cols:
-        print "\t%s - %f" % (attr, entropy)
+        print "%s - %f" % (attr, entropy)
 
-def sort_by_gain(attrcols, entries, ref):
-    # a list of hash tables mapping each class of the domain with the
-    # occourrence inside each attribute:
-    allclasses = [{} for i in xrange(len(attrcols))]
+def collect_attributes(ncols, entries, ref):
+    # ["clima", "dinheiro", "quantos"] (column names)
+    # [{"chuva": [N, {refcount}],
+    #   "sol": [N, {refcount}],
+    #   "frio": [N, {refcount}]},
+    #  {"muito": [N, {refcount}],
+    #   "pouco": [N, {refcount}]},
+    #  {"so": [N, {refcount}],
+    #   "dois": [N, {refcount}],
+    #   "tres": [N, {refcount}],
+    #   "um bando": [N, {refcount}]}]
+    #
+    # refcount counts how many times the class matched a given class in the
+    # "reference" attribute. This is used to obtain the entropy for each
+    # class.  Seems to not be a good representation.
+    # 
+    allclasses = [{} for i in xrange(ncols)]
+    refclasses = {}
     lines = 0
     for items in entries:
-        lines += 1
-        for idx, col in enumerate(items):
+        refclass = items[ref]
+        for idxattr, class_ in enumerate(items):
+            if idxattr == ref:
+                # the only useful information about the classes of the
+                # reference attribute is their frequency
+                refclasses[class_] = refclasses.setdefault(class_, 0) + 1
+                continue
             try:
-                allclasses[idx][col][0] += 1
+                counter = allclasses[idxattr][class_]
             except KeyError:
-                allclasses[idx][col] = [1, [0]*len(attrcols)]
-    entropies = []
+                counter = allclasses[idxattr][class_] = [0, {}]
+            counter[0] += 1
+            counter[1][refclass] = \
+                    counter[1].setdefault(refclass, 0) + 1
+        lines += 1
+    return lines, refclasses, allclasses
+
+def information_gain(attrcols, lines, refclasses, allclasses, ref):
+    # obtain the reference attribute entropy:
+    refentropy = 0.0
     lines = float(lines) # argh!
-    for idx, classes in enumerate(allclasses):
-        #XXX FINISH, add the code to use refcount and separate the code
-        # that handles the global entropy
-        # this code should calculate the entropy of each class of each
-        # attribute
-        entropy = 0.0
-        for class_, (count, refcount) in classes.iteritems():
-            p = count / lines
-            entropy += -p * math.log(p, 2)
-        if idx == ref:
-            # our reference attribute, doesn't need to be sorted
-            refentropy = entropy
+    for class_, count in refclasses.iteritems():
+        p = count / lines
+        refentropy += -p * math.log(p, 2)
+    # calculate the gain for each attribute:
+    for attridx, classes in enumerate(allclasses):
+        if attridx == ref:
             continue
-        entropies.append((entropy, attrcols[idx]))
-    entropies.sort()
-    return entropies
+        attrgain = refentropy
+        for class_, counter in classes.iteritems():
+            classcount = float(counter[0])
+            for refclass, refcount in counter[1].iteritems():
+                p = refcount / classcount
+                attrentropy = p * math.log(p, 2)
+                attrgain -= -(classcount / lines) * attrentropy
+        yield (attrgain, attrcols[attridx])
+
+def sort_by_gain(attrcols, entries, ref):
+    lines, refclasses, allclasses = collect_attributes(len(attrcols), entries, ref)
+    gain = information_gain(attrcols, lines, refclasses, allclasses, ref)
+    return sorted(gain, reverse=True)
         
 def work(args):
     for path in args:
